cf0d06c730e274d0667d22f8b610e1a3fa489be0
khuzamas <khuzamshu@gmail.com>
2019-05-01 09:18:05 +0300

    homework part2

---
+++ models/car.js
@@ -0,0 +1,12 @@
+const mongoose= require('mongoose')
+const Schema= mongoose.Schema
+
+const carSchema= new Schema({
+    name: {type: String, required: true},
+    model: {type: String, required: true},
+    year: {type: Number, required: true},
+    image: String
+}, {timestamps: true})
+
+const Car= mongoose.model('Car', carSchema)
+module.exports= Car
---
+++ models/company.js
@@ -0,0 +1,28 @@
+const mongoose= require('mongoose')
+const Schema= mongoose.Schema
+
+const companySchema= new Schema({
+    name: {type: String, required: true},
+    logo: String,
+    address: {type: String, required: true},
+    city: {type: String, required: true},
+    telephone: {type: Number, required: true},
+    drivers: [{type: Schema.Types.ObjectId, ref: 'Driver'}],
+    cars: [{type: Schema.Types.ObjectId, ref: 'Car'}]
+}, {timestamps: true})
+
+const Compnay= mongoose.model('Company', companySchema)
+module.exports= Compnay
+
+// {
+//     name: {type: String, required: true},
+//     age: {type: Number, required: true},
+//     image: String
+// }
+
+// {
+//     name: {type: String, required: true},
+//     model: {type: String, required: true},
+//     year: {type: Number, required: true},
+//     image: String
+// }
